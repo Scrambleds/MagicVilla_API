07182f26ba0551ac463fbb33fe53d63a08382a96
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2024-05-08 15:11:08 +0700

    API

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
